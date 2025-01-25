f4baf5605cbb6b3a51d9629a5b09e89297be3738
kiko-g <kikojpgoncalves@gmail.com>
2025-01-25 01:09:53 +0000

    Initial Commit

---
+++ Vitalio/VitalioApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalioApp.swift
+//  Vitalio
+//
+//  Created by Francisco Gonçalves on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitalioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitalioTests/VitalioTests.swift
@@ -0,0 +1,17 @@
+//
+//  VitalioTests.swift
+//  VitalioTests
+//
+//  Created by Francisco Gonçalves on 25/01/2025.
+//
+
+import Testing
+@testable import Vitalio
+
+struct VitalioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
